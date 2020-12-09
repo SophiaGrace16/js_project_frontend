0698cc34b642e094709589d35d655228c7b657de
SophiaGrace16 <sophia.g.white@outlook.com>
2020-12-09 13:28:29 -0500

    adding new api call, comment out

--- javascript/services/api.js
+++ javascript/services/api.js
@@ -9,14 +9,20 @@ class API {
             })
         })
     }
-    static addEggs(){
-        fetch("http://localhost:3000/eggs")
-            .then(resp => resp.json())
-            .then(eggs => {
-                eggs.forEach(movie => {
-                    const {id, egg_name, egg, image, movie_id} = egg
-                    new Egg(id, egg_name, egg, image, movie_id)
-            })
-        })
-    }
+
+    // static addEggs(){
+    //     fetch("http://localhost:3000/eggs")
+    //         .then(resp => resp.json())
+    //         .then(eggs => {
+    //             eggs.forEach(egg => {
+    //                 const {id, egg_name, egg, image, movie_id} = egg
+    //                 new Egg(id, egg_name, egg, image, movie_id)
+    //         })
+    //     })
+    // }
 }
+
+// this is a static fetch (class level) request that allows us to call things from our backend. 
+//It will call to the index and it is ultimately what allows us to load in all of our shows. 
+//  const {id, movie_name, date_released, studio_name, image, imdb, movie_link, eggs {id, egg_name, egg, image, movie_id}} = movie
+// new Movie(id, movie_name, date_released, studio_name, image, imdb, movie_link, eggs {id, egg_name, egg, image, movie_id})
